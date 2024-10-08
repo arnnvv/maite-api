732b71e8c5bc1cae44640353f3d4e6eb2e7b15ed
arnnvv <arnav.900@protonmail.com>
2024-10-09 03:07:28 +0530

    otp

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,21 @@ import { eq } from "drizzle-orm";
 import { inferAsyncReturnType } from "@trpc/server";
 import { trpcServer } from "@hono/trpc-server";
 import { appRouter } from "./router";
+import { createTransport, SentMessageInfo } from "nodemailer";
+
+const getGmail = (): string =>
+	process.env.GMAIL ??
+	((): never => {
+		logWithColor("GMAIL is missing!", "\x1b[31m"); // Red
+		throw new Error("Mail not defined");
+	})();
+
+const getGmailPass = (): string =>
+	process.env.GMAIL_PASS ??
+	((): never => {
+		logWithColor("GMAIL_PASS is missing!", "\x1b[31m"); // Red
+		throw new Error("PLZ GET GMAIL PASSWORD");
+	})();
 
 const app = new Hono();
 
@@ -85,6 +100,60 @@ app.post("/check-email", async (c) => {
 		logWithColor(`Error during email check: ${error}`, "\x1b[31m"); // Red
 		return c.json({ error: "Internal server error" }, 500);
 	}
+});
+
+// Send OTP
+app.post("/send-otp", async (c) => {
+	logWithColor("POST /send-otp - Request received", "\x1b[36m"); // Cyan
+	const { email } = await c.req.json();
+
+	if (!email) {
+		logWithColor("Email is required but missing", "\x1b[31m"); // Red
+		return c.json({ error: "Email is required" }, 400);
+	}
+
+	const userExists = await db
+		.select()
+		.from(users)
+		.where(eq(users.email, email))
+		.limit(1);
+
+	if (userExists.length <= 0) {
+		return c.json({ error: "User dosen't Exist" }, 400);
+	}
+
+	const otp = Math.floor(100000 + Math.random() * 900000).toString();
+	logWithColor(`Generated OTP for ${email}: ${otp}`, "\x1b[36m"); // Cyan
+	dummyusers[email] = otp;
+
+	const mailOptions = {
+		from: getGmail(),
+		to: email,
+		subject: "Your OTP",
+		text: `Your OTP is ${otp}`,
+	};
+
+	const transporter = createTransport({
+		service: "gmail",
+		auth: {
+			user: getGmail(),
+			pass: getGmailPass(),
+		},
+	});
+
+	logWithColor(`Attempting to send OTP to ${email}`, "\x1b[33m"); // Yellow
+	transporter.sendMail(
+		mailOptions,
+		(error: Error | null, info: SentMessageInfo): Response | undefined => {
+			if (error) {
+				logWithColor(`Error sending OTP to ${email}: ${error}`, "\x1b[31m"); // Red
+				return c.json({ error: "Error sending OTP" }, 500);
+			} else {
+				logWithColor(`OTP sent to ${email}: ${info.response}`, "\x1b[32m"); // Green
+				return c.json({ message: "OTP sent to email" }, 200);
+			}
+		},
+	);
 });
 
 const port: number = 3000;
