0290980917f14ba690493b874da73ed17788ed82
arnnvv <arnav.900@protonmail.com>
2025-02-05 15:12:43 +0530

    y

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,11 +2,11 @@ import { type Config } from "drizzle-kit";
 import { getDB } from "./lib/db";
 
 export default {
-	schema: "./lib/db/schema.ts",
-	dialect: "postgresql",
-	dbCredentials: {
-		url: getDB(),
-	},
-	tablesFilter: ["peeple_api_"],
-	out: "./lib/db/drizzle",
+  schema: "./lib/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: getDB(),
+  },
+  tablesFilter: ["peeple_api_"],
+  out: "./lib/db/drizzle",
 } satisfies Config;
